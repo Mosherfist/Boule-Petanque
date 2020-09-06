feb465eb74c1e2fafae07b5c9675f4c10fb86d01
juliusheimann <41127120+juliusheimann@users.noreply.github.com>
2020-09-06 11:55:36 +0200

    Update tabelle_abfragen.py

--- tabelle_abfragen.py
+++ tabelle_abfragen.py
@@ -4,7 +4,7 @@ def tabelle_abfragen(datenbank_name):
     stand = []
     conn = sqlite3.connect(datenbank_name + ".db")
     c = conn.cursor()
-    c.execute(""" SELECT Name,  gewonnene_Spiele, Punkte FROM Teilnehmer ORDER by Punkte DESC """)
+    c.execute(""" SELECT Teamname,  gewonnene_Spiele, Punkte FROM Teilnehmer ORDER by Punkte DESC """)
     tabelle = c.fetchall()
     for i in tabelle:
         Name, gewonnene_Spiele, Punkte = i
